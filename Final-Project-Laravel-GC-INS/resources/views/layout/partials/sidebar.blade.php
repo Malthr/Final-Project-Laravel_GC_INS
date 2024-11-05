73678a866da291914fea66861729b43b67af97b5
EfrandiSiregar <riansiregar67@gmail.com>
2024-11-05 13:54:19 +0700

    Update

--- Final-Project-Laravel-GC-INS/resources/views/layout/partials/sidebar.blade.php
+++ Final-Project-Laravel-GC-INS/resources/views/layout/partials/sidebar.blade.php
@@ -1,8 +1,8 @@
-<div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+<div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" style="position: fixed;  width:15%;">
     <a class="d-flex-fill align-items-center p-3 link-dark text-decoration-none border-bottom text-center">
         <span class="fs-3 fw-semibold">Topic</span>
     </a>
-    <div class="list-group list-group-flush overflow-y-scroll" style="max-height: calc(100dvh - 133px);">
+    <div class="list-group list-group-flush overflow-y-auto" style="max-height: calc(100dvh - 133px);">
         @foreach($topics as $topic)
         <a href="{{ route('homepage', ['id_topik' => $topic->id]) }}" class="list-group-item list-group-item-action"
             aria-current="true">
